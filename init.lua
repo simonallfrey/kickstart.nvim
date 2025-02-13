4d33387b87dbd174031d6497dcdcecaef891e6ec
Simon Allfrey <simon.allfrey@gmail.com>
2025-02-13 21:01:36 +0100

    jk with max397574/better-escape.nvim

--- init.lua
+++ init.lua
@@ -174,10 +174,13 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
-vim.keymap.set('t', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
 
-vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Quit insert mode' })
-vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Quit insert mode' })
+-- now use max397574/better-escape
+-- vim.keymap.set('t', 'jk', [[<C-\><C-n>]]) -- no need to escape the '\'
+-- vim.keymap.set('t', 'kj', [[<C-\><C-n>]]) -- no need to escape the '\'
+-- vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Quit insert mode' })
+-- vim.keymap.set('i', 'kj', '<Esc>', { desc = 'Quit insert mode' })
+
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 -- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
@@ -234,6 +237,13 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'bluz71/vim-moonfly-colors',
+  -- lua with lazy.nvim
+  {
+    'max397574/better-escape.nvim',
+    config = function()
+      require('better_escape').setup()
+    end,
+  },
   {
     'NeogitOrg/neogit',
     dependencies = {
